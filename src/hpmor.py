7ce6237b66cae3144e11f8ce6b96d744c5a71965
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2019-07-11 15:06:14 +0200

    prettify and strip inputs

--- src/hpmor.py
+++ src/hpmor.py
@@ -1,18 +1,46 @@
 #!/usr/bin/env python3
 
+import re
+import unicodedata
 from pathlib import Path
 
 import toml
+from bs4 import BeautifulSoup
+
+
+def slugify(value):
+    """
+    Normalizes string, converts to lowercase, removes non-alpha characters,
+    and converts spaces to hyphens.
+
+    https://stackoverflow.com/a/295466/504550
+    """
+    value = unicodedata.normalize("NFKD", value)
+    value = re.sub(r"[^\w\s-]", "", value).strip().lower()
+    value = re.sub(r"[-\s]+", "_", value)
+    return value
 
 
 def relpath(path):
     return Path(__file__).parent / path
 
 
-def process(config, input_path, out_path):
+def process(config, input_path, out_dir):
     with open(input_path) as f:
         data = f.read()
-    # TODO: actually process the files
+
+    soup = BeautifulSoup(data, "html5lib")
+    for strip_selector in config["strip"]["selectors"]:
+        for element in soup.select(strip_selector):
+            element.decompose()
+
+    title = soup.select(config["metadata"]["chapter_title"])[0].get_text()
+    out_fn = Path(f"{int(input_path.stem):03}_{slugify(title)}{input_path.suffix}")
+    print(out_fn)
+    out_path = out_dir / out_fn
+
+    data = soup.prettify()
+
     with open(out_path, "w") as f:
         f.write(data)
 
@@ -28,10 +56,10 @@ def main():
         output_path.unlink()
 
     for input_path in src.glob(f"*.{config['paths']['extension']}"):
-        output_path = relpath(config["paths"]["target"]) / input_path.name
-        if not output_path.parent.exists():
-            output_path.parent.mkdir(parents=True)
-        process(config, input_path, output_path)
+        output_dir = relpath(config["paths"]["target"])
+        if not output_dir.exists():
+            output_dir.mkdir(parents=True)
+        process(config, input_path, output_dir)
 
 
 if __name__ == "__main__":
