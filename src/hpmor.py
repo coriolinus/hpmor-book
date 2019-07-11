d61589355a1c4e1b66ec08f0aedb3bedef1c1990
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2019-07-11 23:34:36 +0200

    excise footnotes without appendicizing

--- src/hpmor.py
+++ src/hpmor.py
@@ -7,6 +7,8 @@ from pathlib import Path
 import toml
 from bs4 import BeautifulSoup
 
+BLOCK_SIZE = 10
+
 
 def slugify(value):
     """
@@ -36,13 +38,18 @@ class Omake(Exception):
 
 def dumpconfig(config):
     i = config["interactive"]
+    r = config["redo"]
     del config["interactive"]
+    del config["redo"]
+
     with open(relpath("config.toml"), "w") as f:
         toml.dump(config, f)
+
     config["interactive"] = i
+    config["redo"] = r
 
 
-def extract_authornote_prefix(config, soup, title):
+def extract_authornote_prefix_and_footnote(config, soup, title):
     try:
         if title in config["omake"]:
             raise Omake()
@@ -57,27 +64,33 @@ def extract_authornote_prefix(config, soup, title):
     content = soup.select(config["metadata"]["story_container"])[0]
     elements = list(content.descendants)
 
-    if n is None and config["interactive"]:
-        block_size = 10
+    def show_elements(block):
+        block_start = BLOCK_SIZE * block
+        block_end = block_start + BLOCK_SIZE
+        for block_idx, element in enumerate(elements[block_start:block_end]):
+            idx = block_start + block_idx
+            el_str = (
+                f"<{element.name}>" if element.name is not None else element.strip()
+            )
+            if len(el_str) == 0:
+                continue
+
+            print(f"{idx:04}: {el_str}")
+        print()
+
+    if config["interactive"] and (config["redo"] or n is None):
         # print descendants in batches of 10, with line numbers, until we're done
+        d = n
+        print()
         print(f"Interactive author-note selection for {title}:")
-        for block in range(len(elements) // block_size):
-            block_start = block_size * block
-            block_end = block_start + block_size
-            for block_idx, element in enumerate(elements[block_start:block_end]):
-                idx = block_start + block_idx
-                el_str = (
-                    f"<{element.name}>" if element.name is not None else element.strip()
-                )
-                if len(el_str) == 0:
-                    continue
+        for block in range(len(elements) // BLOCK_SIZE + 1):
+            show_elements(block)
 
-                print(f"{idx:04}: {el_str}")
-
-            print()
             print("Enter number of first non-author-note line, or")
             print(" n for the next block, or")
             print(" o to declare the whole chaper omake, or")
+            if d is not None:
+                print(f" d to use the current setting ({d})")
             print(" q to exit interactive selection for this file:")
             n = input("> ").strip().lower()
             print()
@@ -87,6 +100,14 @@ def extract_authornote_prefix(config, soup, title):
 
             if n[0] == "o":
                 raise Omake()
+
+            if n[0] == "d":
+                if d is None:
+                    print("no default known!")
+                    continue
+
+                n = d
+                break
 
             if n[0] == "q":
                 return None
@@ -101,6 +122,68 @@ def extract_authornote_prefix(config, soup, title):
 
     if n is None:
         return None
+
+    try:
+        footnote = config["footnotes"][title]
+    except KeyError:
+        footnote = None
+
+    if config["interactive"] and (config["redo"] or footnote is None):
+        # print tail of file in batches of block size, with line numbers, until we're done
+        d = footnote
+        print(f"Interactive footnote selection for {title}")
+        for block in reversed(range(len(elements) // BLOCK_SIZE + 1)):
+            show_elements(block)
+
+            print("Enter number of last story line, or")
+            print(" p for the prev block, or")
+            if d is not None:
+                print(f" d for existing default ({d})")
+            print(" q to exit interactive selection for this file:")
+            footnote = input("> ").strip().lower()
+            print()
+
+            if footnote == "" or footnote[0] == "p":
+                continue
+
+            if footnote[0] == "d":
+                if d is None:
+                    print("no default known!")
+                    continue
+
+                footnote = d
+                break
+
+            if footnote[0] == "q":
+                footnote = -1
+                break
+
+            try:
+                footnote = int(footnote)
+            except ValueError:
+                print("invalid input; aborting")
+                footnote = None
+                break
+
+            if footnote == 0:
+                footnote = -1
+
+            break
+
+    if footnote is not None:
+        config.setdefault("footnotes", {})
+        config["footnotes"][title] = footnote
+
+        # just delete the footnote
+        last_story_element = elements[footnote]
+        while last_story_element.parent is not content:
+            if last_story_element.parent is None:
+                raise Exception("footnote iterated too far; did not stop at content")
+            last_story_element = last_story_element.parent
+        while (
+            len(content.contents) > 0 and content.contents[-1] is not last_story_element
+        ):
+            content.contents.pop()
 
     config.setdefault("author_notes", {})
     config["author_notes"][title] = n
@@ -119,7 +202,7 @@ def extract_authornote_prefix(config, soup, title):
     acontent.contents = []
 
     # update the title
-    chnum, _ = title.split(":")
+    chnum, _ = title.split(":", maxsplit=1)
     appendix_title = f"Appendix A: {chnum} Author's Notes"
     atitle = appendix.select(config["metadata"]["chapter_title"])[0]
     atitle.string = appendix_title
@@ -131,7 +214,7 @@ def extract_authornote_prefix(config, soup, title):
     # move upwards until we have a direct child of content
     while first_story_element.parent is not content:
         if first_story_element.parent is None:
-            raise Exception("iterated too far; did not stop at content")
+            raise Exception("story iterated too far; did not stop at content")
         first_story_element = first_story_element.parent
 
     # now move authors notes into the appendix
@@ -159,7 +242,7 @@ def process(config, input_path, out_dir):
     print(out_fn)
 
     try:
-        appendix = extract_authornote_prefix(config, soup, title)
+        appendix = extract_authornote_prefix_and_footnote(config, soup, title)
     except Omake:
         out_fn = Path(f"appendix_b_{out_fn}")
         config.setdefault("omake", [])
@@ -199,10 +282,8 @@ def main():
     with open(relpath("config.toml")) as f:
         config = toml.load(f)
 
-    if args.redo and "author_notes" in config:
-        del config["author_notes"]
-
     config["interactive"] = args.interactive
+    config["redo"] = args.redo
 
     src = relpath(config["paths"]["source"])
     for output_path in relpath(config["paths"]["target"]).glob(
